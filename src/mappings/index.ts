535d3c93f574ded1da48947c05cbbda24a0b9df6
kfujita0520 <wakyou0520@hotmail.com>
2024-09-08 11:21:01 +0900

    remove erc1155
    erc1155 is not supported

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,5 +1,3 @@
 export {
   handleErc721Transfer,
-  handleErc1155TransferBatch,
-  handleErc1155TransferSingle,
 } from "./transfers";
--- src/mappings/transfers/index.ts
+++ src/mappings/transfers/index.ts
@@ -1,5 +1,1 @@
 export { handleErc721Transfer } from "./nftTransfers/erc721";
-export {
-  handleErc1155TransferBatch,
-  handleErc1155TransferSingle,
-} from "./nftTransfers/erc1155";
